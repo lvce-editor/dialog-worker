bb21730517e98f517da20043b2c6e78c224c5a6d
Le Vivilet <levivilet@pm.me>
2026-07-25 12:33:56 +0000

    fix

--- packages/dialog-worker/src/parts/Process/Process.ts
+++
@@ -1,9 +0,0 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-
-export const { getElectronVersion } = RendererWorker
-
-export const { getNodeVersion } = RendererWorker
-
-export const { getChromeVersion } = RendererWorker
-
-export const { getV8Version } = RendererWorker
