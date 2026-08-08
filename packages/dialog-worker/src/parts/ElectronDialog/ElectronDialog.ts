ff4952f937299a9d2ac371f5c54241420fccbd66
Le Vivilet <levivilet@pm.me>
2026-08-08 07:04:31 +0000

    fix: export dialog message box options type

--- packages/dialog-worker/src/parts/ElectronDialog/ElectronDialog.ts
+++ packages/dialog-worker/src/parts/ElectronDialog/ElectronDialog.ts
@@ -3,7 +3,7 @@ import { MainProcess } from '@lvce-editor/rpc-registry'
 import * as GetWindowId from '../GetWindowId/GetWindowId.ts'
 import * as Product from '../Product/Product.ts'
 
-type ElectronMessageBoxOptions = Omit<MainProcess.ElectronMessageBoxOptions, 'productName' | 'windowId'>
+export type ElectronMessageBoxOptions = Omit<MainProcess.ElectronMessageBoxOptions, 'productName' | 'windowId'>
 
 export const showMessageBox = async (options: ElectronMessageBoxOptions): Promise<number | undefined> => {
   Assert.object(options)
